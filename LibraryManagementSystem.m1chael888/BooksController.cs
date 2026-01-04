474971d7e814a20d5ac97c8ea45bd671cc1072d4
m1chael888 <sofishmain@gmail.com>
2026-01-04 23:03:05 +0200

    BookController

---
+++ LibraryManagementSystem.m1chael888/BooksController.cs
@@ -0,0 +1,73 @@
+﻿using Spectre.Console;
+//// m1chael888 \\\\
+namespace LibraryManagementSystem.m1chael888
+{
+    internal static class BooksController
+    {
+        private static List<string> books = new()
+            {
+                "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
+            };
+
+        internal static void ViewBooks()
+        {
+            AnsiConsole.MarkupLine("[blue]List of Books:[/]\n");
+
+            foreach (var b in books)
+            {
+                AnsiConsole.MarkupLine($"[blue]-[/] {b}");
+            }
+
+            AnsiConsole.MarkupLine("\nPress any key to return to menu...");
+            Console.ReadKey();
+        }
+
+        internal static void AddBook()
+        {
+            var title = AnsiConsole.Ask<string>("[blue]Enter the [white]title[/] of the book you want to add:[/]");
+
+            if (books.Contains(title))
+            {
+                AnsiConsole.MarkupLine("\n[red]That book already exists![/] =(");
+            }
+            else
+            {
+                books.Add(title);
+                AnsiConsole.MarkupLine("\n[green]Your book has been added![/] =)");
+            }
+
+            AnsiConsole.MarkupLine("\nPress any key to return to menu...");
+            Console.ReadKey();
+        }
+
+        internal static void DeleteBook()
+        {
+            if (books.Count() == 0)
+            {
+                AnsiConsole.MarkupLine("[red]There are no books to delete![/]");
+
+                AnsiConsole.MarkupLine("\nPress any key to return to menu...");
+                Console.ReadKey();
+            }
+
+            var bookToDelete = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("[blue]Select a book to delete[/]")
+                .AddChoices(books)
+                .WrapAround(true)
+                .PageSize(25));
+
+            if (books.Remove(bookToDelete))
+            {
+                AnsiConsole.MarkupLine("[green]Book successfuly deleted![/] =)");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Book not found![/] =(");
+            }
+
+            AnsiConsole.MarkupLine("\nPress any key to return to menu...");
+            Console.ReadKey();
+        }
+    }
+}
--- LibraryManagementSystem.m1chael888/Program.cs
+++ LibraryManagementSystem.m1chael888/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Spectre.Console;
-using static System.Reflection.Metadata.BlobBuilder;
+using static LibraryManagementSystem.m1chael888.BooksController;
 //// m1chael888 \\\\
 namespace LibraryManagementSystem.m1chael888
 {
@@ -7,11 +7,6 @@ namespace LibraryManagementSystem.m1chael888
     {
         static void Main(string[] args)
         {
-            var books = new List<string>()
-            {
-                "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
-            };
-
             while (true)
             {
                 Console.Clear();
@@ -38,67 +33,6 @@ namespace LibraryManagementSystem.m1chael888
                         Environment.Exit(0);
                         break;
                 }
-            }
-
-            void ViewBooks()
-            {
-                AnsiConsole.MarkupLine("[blue]List of Books:[/]\n");
-
-                foreach (var b in books)
-                {
-                    AnsiConsole.MarkupLine($"[blue]-[/] {b}");
-                }
-
-                AnsiConsole.MarkupLine("\nPress any key to return to menu...");
-                Console.ReadKey();
-            }
-
-            void AddBook()
-            {
-                var title = AnsiConsole.Ask<string>("[blue]Enter the [white]title[/] of the book you want to add:[/]");
-
-                if (books.Contains(title))
-                {
-                    AnsiConsole.MarkupLine("\n[red]That book already exists![/] =(");
-                }
-                else
-                {
-                    books.Add(title);
-                    AnsiConsole.MarkupLine("\n[green]Your book has been added![/] =)");
-                }
-
-                AnsiConsole.MarkupLine("\nPress any key to return to menu...");
-                Console.ReadKey();
-            }
-
-            void DeleteBook()
-            {
-                if (books.Count() == 0)
-                {
-                    AnsiConsole.MarkupLine("[red]There are no books to delete![/]");
-
-                    AnsiConsole.MarkupLine("\nPress any key to return to menu...");
-                    Console.ReadKey();
-                }
-
-                var bookToDelete = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                    .Title("[blue]Select a book to delete[/]")
-                    .AddChoices(books)
-                    .WrapAround(true)
-                    .PageSize(25));
-
-                if (books.Remove(bookToDelete))
-                {
-                    AnsiConsole.MarkupLine("[green]Book successfuly deleted![/] =)");
-                }
-                else
-                {
-                    AnsiConsole.MarkupLine("[red]Book not found![/] =(");
-                }
-
-                AnsiConsole.MarkupLine("\nPress any key to return to menu...");
-                Console.ReadKey();
             }
         }
 
